a74ecfae81670e91ad316ded340afddbcabf36a3
Kateryna Belchynska <72660010+belchinskaya@users.noreply.github.com>
2022-07-05 20:06:18 +0300

    Update script.js

--- projects/Food/js/script.js
+++ projects/Food/js/script.js
@@ -16,7 +16,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
     tabs('.tabheader__item', '.tabcontent', '.tabheader__items', 'tabheader__item_active');
     modal('[data-modal]', '.modal', modalTimerId);
-    timer('.timer', '2022-06-11');
+    timer('.timer', '2022-10-11');
     cards();
     calc();
     forms('form', modalTimerId);
